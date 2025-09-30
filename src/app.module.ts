442cd5d00472c816cc0a4300256dcf83fd726aa7
gastonbauer <gastonbauerp@gmail.com>
2025-09-30 19:39:13 -0300

    feat: add clients and orders modules with corresponding controllers, services, and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { ClientsModule } from './clients/clients.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, ClientsModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
